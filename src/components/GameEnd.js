f6a1ac86e062e1b0cdabc9802612d0d702c3943c
thomastognacci <thomas.tognacci@gmail.com>
2018-10-21 11:11:40 -0230

    Fixed rebel confettis

--- src/components/GameEnd.js
+++ src/components/GameEnd.js
@@ -1,6 +1,17 @@
 import React from "react";
 import Confetti from "react-confetti";
+import injectSheet from "react-jss";
 
+const style = {
+  confettiContainer: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    pointerEvents: "none",
+  },
+};
 class GameEnd extends React.Component {
   render() {
     const size = {
@@ -8,12 +19,13 @@ class GameEnd extends React.Component {
       height: window.innerHeight,
       numberOfPieces: 350,
     };
+    const {classes} = this.props;
     return (
-      <div style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%"}}>
+      <div className={classes.confettiContainer}>
         <Confetti {...size} />
       </div>
     );
   }
 }
 
-export default GameEnd;
+export default injectSheet(style)(GameEnd);
