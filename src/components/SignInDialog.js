2907c8bac741cdde2efe50ac1e13f80ee64bb61a
Thomas Tognacci <thomas.tognacci@gmail.com>
2018-10-30 18:20:59 -0230

    updated sign in dialog

--- src/components/SignInDialog.js
+++ src/components/SignInDialog.js
@@ -7,75 +7,96 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
-import {Facebook, Twitter, Google, GithubCircle} from "mdi-material-ui";
+import { Facebook, Twitter, Google, GithubCircle } from "mdi-material-ui";
 import PropTypes from "prop-types";
 import DialogActions from "@material-ui/core/DialogActions";
 import Button from "@material-ui/core/Button";
 
 class SignInDialog extends React.PureComponent {
-  handleNotImplemented() {
-    alert("Not implemented yet :(");
-  }
-  render() {
-    const {open, authenticate, handleClose, signIn} = this.props;
-    return (
-      <Dialog onClose={handleClose} open={open}>
-        {signIn ? (
-          <React.Fragment>
-            <DialogTitle id="simple-dialog-title">Please select your sign-in method</DialogTitle>
-            <DialogContent>
-              <DialogContentText>
-                Sign-in to have your best score uploaded and see how you do compare to the rest of
-                the world!
-                <span style={{fonSize: ".5em"}}> and cheaters</span>
-              </DialogContentText>
-            </DialogContent>
-            <List>
-              <ListItem button onClick={authenticate}>
-                <ListItemIcon>
-                  <Facebook />
-                </ListItemIcon>
-                <ListItemText primary="Facebook" />
-              </ListItem>
-              <ListItem button onClick={this.handleNotImplemented}>
-                <ListItemIcon>
-                  <Twitter />
-                </ListItemIcon>
-                <ListItemText primary="Twitter" />
-              </ListItem>
-              <ListItem button onClick={this.handleNotImplemented}>
-                <ListItemIcon>
-                  <Google />
-                </ListItemIcon>
-                <ListItemText primary="Google" />
-              </ListItem>
-              <ListItem button onClick={this.handleNotImplemented}>
-                <ListItemIcon>
-                  <GithubCircle />
-                </ListItemIcon>
-                <ListItemText primary="GitHub" />
-              </ListItem>
-            </List>
-          </React.Fragment>
-        ) : (
-          <React.Fragment>
-            <DialogTitle id="simple-dialog-title">Signed out</DialogTitle>
-            <DialogContent>
-              <DialogContentText>You are no longer signed-in.</DialogContentText>
-            </DialogContent>
-            <DialogActions>
-              <Button onClick={handleClose}>Ok</Button>
-            </DialogActions>
-          </React.Fragment>
-        )}
-      </Dialog>
-    );
-  }
+	handleNotImplemented() {
+		alert("Not implemented yet :(");
+	}
+	render() {
+		const { open, authenticate, handleClose, signIn, isSignedIn } = this.props;
+		return (
+			<Dialog onClose={handleClose} open={open}>
+				{signIn ? (
+					<React.Fragment>
+						<DialogTitle id="simple-dialog-title">
+							{isSignedIn ? "Success" : "Please select your sign-in method"}
+						</DialogTitle>
+						{isSignedIn ? (
+							<DialogContent>
+								<DialogContentText>
+									Your best score will be uploaded automatically in the
+									ladderboard
+								</DialogContentText>
+							</DialogContent>
+						) : (
+							<React.Fragment>
+								<DialogContent>
+									<DialogContentText>
+										Sign-in to have your best score uploaded and see how you do
+										compare to the rest of the world!{" "}
+										<span style={{ fontSize: ".5em" }}> and cheaters</span>
+									</DialogContentText>
+								</DialogContent>
+								<List>
+									<ListItem button onClick={authenticate}>
+										<ListItemIcon>
+											<Facebook />
+										</ListItemIcon>
+										<ListItemText primary="Facebook" />
+									</ListItem>
+									<ListItem button onClick={this.handleNotImplemented}>
+										<ListItemIcon>
+											<Twitter />
+										</ListItemIcon>
+										<ListItemText primary="Twitter" />
+									</ListItem>
+									<ListItem button onClick={this.handleNotImplemented}>
+										<ListItemIcon>
+											<Google />
+										</ListItemIcon>
+										<ListItemText primary="Google" />
+									</ListItem>
+									<ListItem button onClick={this.handleNotImplemented}>
+										<ListItemIcon>
+											<GithubCircle />
+										</ListItemIcon>
+										<ListItemText primary="GitHub" />
+									</ListItem>
+								</List>
+							</React.Fragment>
+						)}
+						<DialogActions>
+							<Button color="primary" onClick={handleClose}>
+								Close
+							</Button>
+						</DialogActions>
+					</React.Fragment>
+				) : (
+					<React.Fragment>
+						<DialogTitle id="simple-dialog-title">Signed out</DialogTitle>
+						<DialogContent>
+							<DialogContentText>
+								You are no longer signed-in.
+							</DialogContentText>
+						</DialogContent>
+						<DialogActions>
+							<Button onClick={handleClose}>Ok</Button>
+						</DialogActions>
+					</React.Fragment>
+				)}
+			</Dialog>
+		);
+	}
 }
 SignInDialog.propTypes = {
-  authenticate: PropTypes.func,
-  signOutHandler: PropTypes.func,
-  open: PropTypes.bool.isRequired,
-  signIn: PropTypes.bool,
+	authenticate: PropTypes.func,
+	signOutHandler: PropTypes.func,
+	open: PropTypes.bool.isRequired,
+	signIn: PropTypes.bool,
+	isSignedIn: PropTypes.bool
 };
 export default SignInDialog;
