8f826ac19b0bc5ff4045a8887434e276190c7e5c
thomastognacci <thomas.tognacci@gmail.com>
2018-10-31 11:19:07 -0230

    score format

--- src/components/OnlineScores.js
+++ src/components/OnlineScores.js
@@ -7,204 +7,220 @@ import "firebase/auth";
 import ScoreList from "./ScoreList";
 import base from "../base";
 import SignIn from "./SignIn";
-import { firebaseApp } from "../base";
+import {firebaseApp} from "../base";
 import Button from "@material-ui/core/Button";
-import { ViewList } from "mdi-material-ui";
+import {ViewList} from "mdi-material-ui";
 
 import FullPlayerListDialog from "./FullPlayerListDialog";
 
 const style = {
-	fullList: {
-		marginRight: "1rem",
-	},
-	fullListIcon: {
-		marginLeft: ".5rem",
-	},
-	onlineScoreActions: {
-		display: "flex",
-		flexWrap: "wrap",
-		justifyContent: "center",
-		marginBottom: "1rem",
-	},
+  fullList: {
+    marginRight: "1rem",
+  },
+  fullListIcon: {
+    marginLeft: ".5rem",
+  },
+  onlineScoreActions: {
+    display: "flex",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    marginBottom: "1rem",
+  },
 };
 class OnlineScores extends React.PureComponent {
-	state = {
-		scores: null,
-		onlineScores: {
-			bestScore: null,
-			secondBestScore: null,
-			thirdBestScore: null,
-		},
-		fullPlayerListOpen: false,
-		fullPlayerList: [],
-		fetchError: false,
-		user: null,
-		isSignedIn: false,
-	};
+  state = {
+    scores: null,
+    onlineScores: {
+      bestScore: null,
+      secondBestScore: null,
+      thirdBestScore: null,
+    },
+    fullPlayerListOpen: false,
+    fullPlayerList: [],
+    fetchError: false,
+    user: null,
+    isSignedIn: false,
+  };
 
-	updateOnlineScores = scores => {
-		// If no score or error connecting to the DB
-		if (!Array.isArray(scores)) return this.setState({ fetchError: true });
+  updateOnlineScores = (scores) => {
+    // If no score or error connecting to the DB
+    if (!Array.isArray(scores)) return this.setState({fetchError: true});
 
-		// Order the scores and grab the 3 best
-		let scoreOrdered = scores.slice();
-		scoreOrdered.sort((a, b) => {
-			return b.score - a.score;
-		});
-		let bestScore = scoreOrdered[0];
-		let secondBestScore = scoreOrdered[1];
-		let thirdBestScore = scoreOrdered[2];
+    // Order the scores and grab the 3 best
+    let scoreOrdered = scores.slice();
+    scoreOrdered.sort((a, b) => {
+      return b.score - a.score;
+    });
+    let bestScore = scoreOrdered[0];
+    let secondBestScore = scoreOrdered[1];
+    let thirdBestScore = scoreOrdered[2];
 
-		const onlineScores = { bestScore, secondBestScore, thirdBestScore };
-		return this.setState({ onlineScores, fullPlayerList: scoreOrdered });
-	};
+    const onlineScores = {bestScore, secondBestScore, thirdBestScore};
+    return this.setState({onlineScores, fullPlayerList: scoreOrdered});
+  };
 
-	handleClick = () => {
-		this.setState({ fullPlayerListOpen: true });
-	};
+  handleClick = () => {
+    this.setState({fullPlayerListOpen: true});
+  };
 
-	handleClose = () => {
-		this.setState({ fullPlayerListOpen: false });
-	};
+  handleClose = () => {
+    this.setState({fullPlayerListOpen: false});
+  };
 
-	authHandler = async data => {
-		const user = {
-			uid: data.user.uid,
-			photoURL: data.user.photoURL,
-			name: data.user.displayName,
-		};
+  authHandler = async (data) => {
+    const user = {
+      uid: data.user.uid,
+      photoURL: data.user.photoURL,
+      name: data.user.displayName,
+    };
 
-		// if there is a best score already, push it online
-		this.pushBestScore();
+    // if there is a best score already, push it online
+    this.pushBestScore();
 
-		this.setState({ isSignedIn: true, user });
-	};
+    this.setState({isSignedIn: true, user});
+  };
 
-	signOutHandler = async () => {
-		await firebase.auth().signOut();
-		this.setState({ isSignedIn: false, user: null });
-	};
+  signOutHandler = async () => {
+    await firebase.auth().signOut();
+    this.setState({isSignedIn: false, user: null});
+  };
 
-	authenticate = () => {
-		const authProvider = new firebase.auth.FacebookAuthProvider();
-		firebaseApp
-			.auth()
-			.signInWithPopup(authProvider)
-			.then(this.authHandler);
-	};
+  authenticate = () => {
+    const authProvider = new firebase.auth.FacebookAuthProvider();
+    firebaseApp
+      .auth()
+      .signInWithPopup(authProvider)
+      .then(this.authHandler);
+  };
 
-	fetchDBEntries() {
-		base.fetch("scores", {
-			context: this,
-			asArray: true,
-			then(data) {
-				this.updateOnlineScores(data);
-			},
-		});
-	}
+  fetchDBEntries() {
+    base.fetch("scores", {
+      context: this,
+      asArray: true,
+      then(data) {
+        this.updateOnlineScores(data);
+      },
+    });
+  }
 
-	pushBestScore = () => {
-		const { bestScore } = this.props.localScores;
-		const { fullPlayerList } = this.state;
-		if (!bestScore) return;
+  pushBestScore = () => {
+    const {bestScore} = this.props.localScores;
+    const {fullPlayerList} = this.state;
+    if (!bestScore) return;
 
-		const { user } = this.state;
+    const {user} = this.state;
 
-		if (!this.state.isSignedIn || !user.uid) return;
+    if (!this.state.isSignedIn || !user.uid) return;
 
-		const newEntry = {
-			score: bestScore.score,
-			date: bestScore.secondary.toString(),
-			uid: user.uid,
-			photoURL: user.photoURL,
-			name: user.name,
-		};
+    const newEntry = {
+      score: bestScore.score,
+      date: bestScore.secondary.toString(),
+      uid: user.uid,
+      photoURL: user.photoURL,
+      name: user.name,
+    };
 
-		if (Array.isArray(fullPlayerList)) {
-			// Check if user already has a score in the DB, and if the new best score is higher than the previous one
-			if (fullPlayerList.length !== 0) {
-				var dbEntry = fullPlayerList.find(function(player) {
-					return player.uid === user.uid;
-				});
+    console.debug("New best score");
+    if (Array.isArray(fullPlayerList)) {
+      // Check if user already has a score in the DB, and if the new best score is higher than the previous one
+      if (fullPlayerList.length !== 0) {
+        var dbEntry = fullPlayerList.find(function(player) {
+          return player.uid === user.uid;
+        });
 
-				if (dbEntry) {
-					// skip the entry is user already in DB but the score is lower than the previous one
-					if (dbEntry.score > newEntry.score) {
-						return;
-					} else {
-						return base.update(`scores/${dbEntry.key}`, {
-							data: newEntry,
-							then(err) {
-								if (err) return console.error("Error: ", err);
-							},
-						});
-					}
-				}
-			}
-			// otherwise, create new entry
-			return base.push("scores", {
-				data: newEntry,
-				then(err) {
-					if (err) return console.error("Error: ", err);
-				},
-			});
-		}
-	};
+        if (dbEntry) {
+          console.debug("Player exist!");
+          // skip the entry is user already in DB but the score is lower than the previous one
+          if (dbEntry.score > newEntry.score) {
+            console.debug("Score is lower than the one in the DB, return");
+            return;
+          } else {
+            console.debug("Updated the score for that player");
+            return base.update(`scores/${dbEntry.key}`, {
+              data: newEntry,
+              then(err) {
+                if (err) return console.error("Error: ", err);
+              },
+            });
+          }
+        }
+      }
+      // otherwise, create new entry
+      console.debug("New player entered");
+      return base.push("scores", {
+        data: newEntry,
+        then(err) {
+          if (err) return console.error("Error: ", err);
+        },
+      });
+    }
+  };
 
-	componentDidMount() {
-		firebase.auth().onAuthStateChanged(user => {
-			if (user) this.authHandler({ user });
-		});
-		this.fetchDBEntries();
-	}
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) this.authHandler({user});
+    });
+    this.fetchDBEntries();
+  }
 
-	componentDidUpdate(prevProps) {
-		if (prevProps.localScores.bestScore !== this.props.localScores.bestScore) {
-			this.fetchDBEntries();
-			this.pushBestScore();
-		}
-	}
+  componentDidUpdate(prevProps) {
+    if (prevProps.localScores.bestScore !== this.props.localScores.bestScore) {
+      this.fetchDBEntries();
+      this.pushBestScore();
+    }
+  }
 
-	render() {
-		const { classes } = this.props;
-		const { onlineScores, isSignedIn, fullPlayerList, fullPlayerListOpen, fetchError, user } = this.state;
-		return (
-			<React.Fragment>
-				<ScoreList fetchError={fetchError} online {...onlineScores} />
-				<div className={classes.onlineScoreActions}>
-					{/* // TODO Move to its own component */}
-					<Button
-						color="primary"
-						className={classes.fullList}
-						size="small"
-						onClick={this.handleClick}
-						disabled={fetchError}
-					>
-						View Full List
-						<ViewList className={classes.fullListIcon} />
-					</Button>
-					<FullPlayerListDialog
-						user={user}
-						handleClose={this.handleClose}
-						open={fullPlayerListOpen}
-						fullPlayerList={fullPlayerList}
-					/>
-					{/* // TODO /end Move to its own component */}
+  render() {
+    const {classes} = this.props;
+    const {
+      onlineScores,
+      isSignedIn,
+      fullPlayerList,
+      fullPlayerListOpen,
+      fetchError,
+      user,
+    } = this.state;
+    return (
+      <React.Fragment>
+        <ScoreList fetchError={fetchError} online {...onlineScores} />
+        <div className={classes.onlineScoreActions}>
+          {/* // TODO Move to its own component */}
+          <Button
+            color="primary"
+            className={classes.fullList}
+            size="small"
+            onClick={this.handleClick}
+            disabled={fetchError}
+          >
+            View Full List
+            <ViewList className={classes.fullListIcon} />
+          </Button>
+          <FullPlayerListDialog
+            user={user}
+            handleClose={this.handleClose}
+            open={fullPlayerListOpen}
+            fullPlayerList={fullPlayerList}
+          />
+          {/* // TODO /end Move to its own component */}
 
-					<SignIn signOutHandler={this.signOutHandler} isSignedIn={isSignedIn} authenticate={this.authenticate} />
-				</div>
-			</React.Fragment>
-		);
-	}
+          <SignIn
+            signOutHandler={this.signOutHandler}
+            isSignedIn={isSignedIn}
+            authenticate={this.authenticate}
+          />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 OnlineScores.propTypes = {
-	handleLocalScores: PropTypes.func.isRequired,
-	localScores: PropTypes.shape({
-		bestScore: PropTypes.object,
-		secondBestScore: PropTypes.object,
-		thirdBestScore: PropTypes.object,
-	}),
+  handleLocalScores: PropTypes.func.isRequired,
+  localScores: PropTypes.shape({
+    bestScore: PropTypes.object,
+    secondBestScore: PropTypes.object,
+    thirdBestScore: PropTypes.object,
+  }),
 };
 
 export default injectSheet(style)(OnlineScores);
--- src/helpers/formatScore.js
+++ src/helpers/formatScore.js
@@ -2,10 +2,12 @@ export function formatScore(score) {
   let scoreString = score.toString();
   if (scoreString.length < 4) return score;
   if (scoreString.length < 7) {
-    return `${scoreString.slice(0, -3)}.${scoreString.slice(-3)}`;
+    return `${scoreString.slice(0, -3)}\xa0${scoreString.slice(-3)}`;
   }
   if (scoreString.length < 10) {
-    return `${scoreString.slice(-9, -6)}.${scoreString.slice(-6, -3)}.${scoreString.slice(-3)}`;
+    return `${scoreString.slice(-9, -6)}\xa0${scoreString.slice(-6, -3)}\xa0${scoreString.slice(
+      -3,
+    )}`;
   }
   return "cheater!";
 }
